6fcdb6ae5380567bc1fa47289bf8048c41cc4cd1
rdar70993-cyber <rdar70993@gmail.com>
2025-12-03 10:58:32 +0330

    Create shaping ap.py

---
+++ shaping ap.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class StoreApp(App):
+    def build(self):
+        return Label(text="Welcome to Store AP")
+
+if __name__ == "__main__":
+    StoreApp().run()
